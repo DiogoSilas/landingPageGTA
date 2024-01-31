7730d0b9cdd83b63c95641846b3e95269d2e5dc2
Diogo Silas <diogosilas4@gmail.com>
2024-01-31 18:32:36 -0300

    Melhoria na lógica do código.

--- assets/scripts/javascript/index.js
+++ assets/scripts/javascript/index.js
@@ -1,11 +1,7 @@
-const btnOpenAndClosePlatforms = window.document.getElementsByClassName('btn-plataforma');
-const platforms = window.document.getElementsByClassName('plataformas');
-console.log(platforms[1]);
+const btnOpenAndClosePlatforms = document.querySelector('.btn-plataforma');
+const platforms = document.querySelector('.btn-plataforma .plataformas');
 
-btnOpenAndClosePlatforms[0].addEventListener('click', () => {
-    if(platforms[1].classList.contains('ativo')) {
-        platforms[1].classList.remove('ativo');
-    } else if(!platforms[1].classList.contains('ativo')) {
-        platforms[1].classList.add('ativo');
-    };
+btnOpenAndClosePlatforms.addEventListener('click', () => {
+    platforms.classList.toggle('ativo');
 });
+
